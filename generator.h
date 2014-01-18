7a615c0808291a58e5dc8a7d6ba1b19b75474e47
justyna <j.mucha.official@gmail.com>
2014-01-18 13:27:00 +0100

    Removed files

--- generator.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int[] generate() {
-
-	int tab[] = {2,5,84,25,44,24,66,2,6,1};
-
-	return tab;
-}
--- min.h
+++
@@ -1,8 +0,0 @@
-int min(int tab[]) {
-	int temp = tab[0];
-	for (int i = 1; i < 10; i++) {
-		if tab[i] < temp
-			temp = tab[i];
-	}
-	return temp;
-}
